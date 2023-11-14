ad64bafa282b4c43a2812640693293ba8b36ce0d
Pablo Bollero <pablo.bolleroh@gmail.com>
2023-11-14 21:48:09 +0000

    Credentials

--- src/Components/Firebase/Firebase.js
+++ src/Components/Firebase/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBYDv_kT74wbxMI2MVXl6GT80IB2TiycB8",
-    authDomain: "proyecto-final-react-74248.firebaseapp.com",
-    projectId: "proyecto-final-react-74248",
-    storageBucket: "proyecto-final-react-74248.appspot.com",
-    messagingSenderId: "201020294240",
-    appId: "1:201020294240:web:89e28477aaac41114f2f05"
-};
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
+}
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
