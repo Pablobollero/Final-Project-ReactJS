f0caf094a4f2a0b03848c36f8865a1a408102e16
Pablo Bollero <pablo.bolleroh@gmail.com>
2023-11-15 01:21:20 +0000

    ready

--- src/Components/ItemDetail/ItemDetail.js
+++ src/Components/ItemDetail/ItemDetail.js
@@ -1,60 +1,64 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import ItemCount from '../ItemCount/ItemCount'
-import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
-import { CartContext } from '../Context/CartContext';
-import './ItemDetail.css';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import ItemCount from "../ItemCount/ItemCount";
+import "./ItemDetail.css";
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../Context/CartContext";
 
-const ItemDetail = ({id, img, title, description, price, stock}) => {
-    const [quantityAdded, setQuantityAdded] = React.useState(0)
+const ItemDetail = ({id, name, img, category, description, price, stock}) => {
+    const [quantityAdded, setQuantityAdded] = useState(0)
 
-    const { addItem } = React.useContext(CartContext)
-
+    const { addItem } = useContext(CartContext)
+    
     const handleOnAdd = (quantity) => {
         setQuantityAdded(quantity)
 
         const item = {
-            id, title, price
+            id, name, price
         }
+
         addItem(item, quantity)
     }
 
     return (
-        <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-            component="img"
-            sx={{ height: 345 }}
-            image={img}
-        />
-        <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            {title}
-            </Typography>
-            <Typography variant="body2" color="warning.light">
-            {description}
-            </Typography>
-        </CardContent>
-        <CardContent>    
-            <Typography variant="body2" color="text.secondary">
-            Precio: £{price}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-            Stock: {stock}
-            </Typography>
-        </CardContent>
-        <CardActions className='terminarCompra'>
-            {
-                quantityAdded > 0 ? (
-                    <Link to='/cart' className='btn btn-success btn-sm'>Terminar Compra</Link>
-                ) : (
-                    <ItemCount initial={1} stock={stock} onAdd={handleOnAdd}/>
-                )
-            }
-        </CardActions>
+        <Card className="card" sx={{  maxWidth: 345, borderRadius: '9rem', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', backgroundColor: '#f3f3f3' }}>
+            <CardMedia
+                component="img"
+                sx={{ height: 345 }}
+                image={img}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                </Typography>
+                <Typography variant="body2" color="warning.light">
+                    {description}
+                </Typography>
+            </CardContent>
+            <CardContent>    
+                <Typography variant="body2" color="text.secondary">
+                    Precio: £{price}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Stock: {stock}
+                </Typography>
+            </CardContent>
+            <CardActions className='count'>
+                {
+                    quantityAdded > 0 ? (
+                        <>
+                            <Link to='/cart' className="Option btn btn-success btn-sm">Finalizar Compra</Link>
+                            <Link to='/productos' className="Option btn btn-success btn-sm">Seguir Comprando</Link>
+                        </>
+                    ) : (
+                        <ItemCount initial={1} stock={stock} onAdd={handleOnAdd} />
+                    )
+                }
+            </CardActions>
         </Card>
     )
 };
